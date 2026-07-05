9700fe43ba6bcab298a0eb9c8b2e47729a492e96
Azael1902 <jazael1902@gmail.com>
2026-07-04 23:03:43 -0600

    Implementa módulo de certificados

--- certificates/admin.py
+++ certificates/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Certificado
 
-# Register your models here.
+
+@admin.register(Certificado)
+class CertificadoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'numero_serie',
+        'rfc',
+        'fecha_expiracion',
+        'fecha_registro',
+    )
+
+    search_fields = (
+        'numero_serie',
+        'rfc',
+        'curp',
+    )
--- certificates/models.py
+++ certificates/models.py
@@ -2,6 +2,27 @@ from django.db import models
 
 
 class Certificado(models.Model):
+    archivo_cer = models.FileField(upload_to='certificados/cer/')
+    archivo_key = models.FileField(upload_to='certificados/key/')
+
+    numero_serie = models.CharField(max_length=255, blank=True)
+    subject = models.TextField(blank=True)
+    issuer = models.TextField(blank=True)
+
+    rfc = models.CharField(max_length=20, blank=True)
+    curp = models.CharField(max_length=30, blank=True)
+
+    fecha_inicio = models.DateTimeField(null=True, blank=True)
+    fecha_expiracion = models.DateTimeField(null=True, blank=True)
+
+    algoritmo_firma = models.CharField(max_length=255, blank=True)
+    huella_sha256 = models.CharField(max_length=255, blank=True)
+
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         verbose_name = 'Certificado'
         verbose_name_plural = 'Certificados'
+
+    def __str__(self):
+        return self.numero_serie or f'Certificado {self.id}'
